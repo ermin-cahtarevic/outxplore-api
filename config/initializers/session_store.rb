9bea1d30479bce97f2c03c1113d13bd0de298d71
Ermin Cahtarevic <erminc@windowslive.com>
2020-10-29 18:13:36 +0100

    Update session store url

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_outxplore", domain: "outxplore-api.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_outxplore", domain: "https://nameless-waters-17056.herokuapp.com/"
 else
   Rails.application.config.session_store :cookie_store, key: "_outxplore"
 end
