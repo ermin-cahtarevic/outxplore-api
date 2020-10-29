4a303271d41a8ff1d9c6d70c678dbdae69ed3241
Ermin Cahtarevic <erminc@windowslive.com>
2020-10-29 20:15:44 +0100

    Update CORS origin url

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_outxplore", domain: "https://nameless-waters-17056.herokuapp.com/"
+  Rails.application.config.session_store :cookie_store, key: "_outxplore", same_site: :none, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: "_outxplore"
 end
